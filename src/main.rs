c3a9bb2852cd9914358312aff2fff1df24d212c1
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-07-31 12:28:38 -0600

    compiling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 
-use std::error::Error;
+use std::{error::Error};
+use core::{bindings::arbiter_token::ArbiterToken, manager::SimulationManager};
 
-pub fn run() -> Result<(), Box<dyn Error>> {
-    todo!()
+mod bindings;
+
+const TEST_ENV_LABEL: &str = "test";
+const TEST_AGENT_NAME: &str = "test_agent";
+const TEST_ARG_NAME: &str = "test_arg";
+const TEST_ARG_SYMBOL: &str = "TEST";
+const TEST_ARG_DECIMALS: u8 = 18;
+
+#[tokio::main]
+pub async fn run() -> Result<(), Box<dyn Error>> {
+    // let mut environment = Environment::new(TEST_ENV_LABEL.to_string());
+    let mut manager = SimulationManager::new();
+    manager.add_environment(TEST_ENV_LABEL.clone().to_string())?;
+    let mut environment = manager.environments.get_mut(TEST_ENV_LABEL).unwrap();
+    environment.add_agent(TEST_AGENT_NAME.to_string(), environment.connection.clone());
+    let agent = environment.clients[0].clone();
+    let address = ArbiterToken::deploy(agent.client, (
+            TEST_ARG_NAME.to_string(),
+            TEST_ARG_SYMBOL.to_string(),
+            TEST_ARG_DECIMALS,
+        ),
+    )?
+    .send().await?;
 }
