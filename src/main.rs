6e9e5f6868b093e4934a9df31ded7eb242f7552e
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-07-31 12:43:16 -0600

    Counter trait bound not satisfied

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 
 use std::{error::Error};
-use core::{bindings::arbiter_token::ArbiterToken, manager::SimulationManager};
+use arbiter_core::{bindings::arbiter_token::ArbiterToken, manager::SimulationManager};
+
+use crate::bindings::counter::Counter;
 
 mod bindings;
 
@@ -11,18 +13,30 @@ const TEST_ARG_SYMBOL: &str = "TEST";
 const TEST_ARG_DECIMALS: u8 = 18;
 
 #[tokio::main]
-pub async fn run() -> Result<(), Box<dyn Error>> {
-    // let mut environment = Environment::new(TEST_ENV_LABEL.to_string());
+pub async fn main() -> Result<(), Box<dyn Error>> {
+    // Create a new simulation manager
     let mut manager = SimulationManager::new();
+    // Add a new environment
     manager.add_environment(TEST_ENV_LABEL.clone().to_string())?;
     let mut environment = manager.environments.get_mut(TEST_ENV_LABEL).unwrap();
-    environment.add_agent(TEST_AGENT_NAME.to_string(), environment.connection.clone());
-    let agent = environment.clients[0].clone();
-    let address = ArbiterToken::deploy(agent.client, (
+    // Add a new agent to the environment
+    environment.add_agent(TEST_AGENT_NAME.to_string());
+    let agent = environment.clients.get(TEST_AGENT_NAME).unwrap();
+    // Deploy a new ArbiterToken contract
+    let token = ArbiterToken::deploy(agent.client, (
             TEST_ARG_NAME.to_string(),
             TEST_ARG_SYMBOL.to_string(),
             TEST_ARG_DECIMALS,
         ),
     )?
     .send().await?;
+    println!("Deployed ArbiterToken to address: {}", token.address());
+
+    // deploy counter contract
+    let counter = Counter::deploy(agent.client, ())?.send().await?;
+    println!("Deployed Counter to address: {}", counter.address());
+    
+
+
+    Ok(())
 }
