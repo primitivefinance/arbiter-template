5b2d42af0b2037dc47a5bb2106a81cd3b5f84e82
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-08-03 13:31:57 -0700

    here we go

--- src/behaviors/mod.rs
+++ src/behaviors/mod.rs
@@ -1,15 +1,30 @@
-use arbiter_core::agent::Behavior;
 
-struct MyBehavior {
-    name: String,
-}
 
-#[async_trait::async_trait]
-impl Behavior for MyBehavior {
-    async fn process_event(&mut self) -> bool {
-        todo!()
-    }
-    fn sync_state(&mut self) {
-        todo!()
-    }
-}
+
+// struct DeployBehavior {
+//     name: String,
+// }
+
+// #[async_trait::async_trait]
+// impl Behavior for DeployBehavior {
+//     async fn process_event(&mut self) -> bool {
+//         self.deploy();
+//         true
+//     }
+//     fn sync_state(&mut self) {
+//         todo!()
+//     }
+// }
+
+// impl DeployBehavior {
+//     pub fn new(name: String) -> Self {
+//         Self { name }
+//     }
+
+//     fn deploy(&self) {
+//         // deploy counter contract
+//         let counter = Counter::deploy(self.client.clone(), ())?.send().await?;
+//         println!("Deployed Counter to address: {}", counter.address());
+//     }
+// }
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
-use std::{error::Error, env};
-use arbiter_core::{bindings::arbiter_token::ArbiterToken, manager::SimulationManager, agent::Agent};
+use std::{error::Error};
+use arbiter_core::{manager::SimulationManager, agent::{Agent}};
 
 use crate::bindings::counter::Counter;
 
@@ -8,7 +8,7 @@ mod bindings;
 mod behaviors;
 
 const TEST_ENV_LABEL: &str = "test";
-const TEST_AGENT_NAME: &str = "test_agent";
+const TEST_AGENT_NAME: &str = "bob";
 const TEST_ARG_NAME: &str = "test_arg";
 const TEST_ARG_SYMBOL: &str = "TEST";
 const TEST_ARG_DECIMALS: u8 = 18;
@@ -19,16 +19,30 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
     let mut manager = SimulationManager::new();
     // Add a new environment
     manager.add_environment(TEST_ENV_LABEL.clone().to_string())?;
-    manager.configure_lambda(100.00, TEST_ENV_LABEL.clone().to_string())?;
     manager.run_environment(TEST_ENV_LABEL.clone().to_string())?;
     // Add a new agent to the environment
-    manager.add_agent(TEST_AGENT_NAME.to_string(), TEST_ENV_LABEL.clone().to_string())?;
+    // manager.add_agent(TEST_AGENT_NAME.to_string(), TEST_ENV_LABEL.clone().to_string())?;
+    let bob = Agent::new(TEST_AGENT_NAME.to_string());
+    // let deploy_behavior = behaviors::DeployBehavior::new("deploy".to_string());
+    // bob.add_behavior(deploy_behavior);
+
+    manager.add_agent(bob, TEST_ENV_LABEL.clone().to_string())?;
+
+    let bob = manager.environments.get_mut(&TEST_ENV_LABEL.clone().to_string()).unwrap().agents.get(0).unwrap();
+
+    let counter = Counter::deploy(bob.client.clone(), ())?.send().await?;
+
+    println!("Deployed Counter to address: {}", counter.address());
+
+    manager.environments.get_mut(&TEST_ENV_LABEL.clone().to_string()).unwrap();
 
     // TODO Need to create a new agent and add it to the environment
     // maybe something like this: 
     // let bob = Agent::new("name".to_owned(), manager.environments.get(&TEST_ENV_LABEL.clone().to_string()));
-    // bob.add_behavior("test".to_owned(), behaviors::TestBehavior::new());
-    // Probably read up on artemis
+    // bob.add_behavior(behaviors::TestBehavior::new());
+    // Probably read up on artemis 
+    // https://github.com/paradigmxyz/artemis/blob/c8ab223a363a875f685ab177839eacfffc9d8de0/crates/artemis-core/src/types.rs#L25
+
 
 
     // Deploy a new ArbiterToken contract
@@ -41,8 +55,6 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
     // .send().await?;
     // println!("Deployed ArbiterToken to address: {}", token.address());
 
-    // // deploy counter contract
-    // let counter = Counter::deploy(agent.client.clone(), ())?.send().await?;
-    // println!("Deployed Counter to address: {}", counter.address());
+
     Ok(())
 }
