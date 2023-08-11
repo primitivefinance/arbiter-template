8feccce5bdaf51fbfda6202dae11f583ff58cfd8
kinrezc <matt.czernik@gmail.com>
2023-08-11 17:45:54 -0400

    wip

--- src/strategies.rs
+++ src/strategies.rs
@@ -1,38 +1,72 @@
-use std::sync::Arc;
+use arbiter_core::strategies::{SimulationActions, SimulationEvents};
+use arbiter_core::{environment::RevmResult, middleware::RevmMiddleware};
 use artemis_core::types::Strategy;
-use arbiter_core::{middleware::RevmMiddleware, environment::RevmResult};
+use std::sync::Arc;
 
-use async_trait::async_trait;
-use ethers::providers::Middleware;
-use anyhow::Result;
 use crate::bindings::counter::Counter;
+use anyhow::Result;
+use async_trait::async_trait;
 
-
-pub struct DeployStrategy<M> {
+pub struct CounterStrategy {
     label: String,
-    client: Arc<M>,
+    client: Arc<RevmMiddleware>,
+    counter: Option<Counter<RevmMiddleware>>,
+    sender: crossbeam_channel::Sender<SimulationEvents>,
 }
 
-impl<M: Middleware + 'static> DeployStrategy<M> {
-    pub fn new(label: String, client: Arc<M>) -> Self {
+impl CounterStrategy {
+    pub fn new(
+        label: String,
+        client: Arc<RevmMiddleware>,
+        sender: crossbeam_channel::Sender<SimulationEvents>,
+    ) -> Self {
         Self {
             label,
             client,
+            counter: None,
+            sender,
         }
     }
 }
 
 #[async_trait]
-impl<M: Middleware + 'static> Strategy<RevmResult, ()> for DeployStrategy<M> {
+impl Strategy<SimulationEvents, SimulationActions> for CounterStrategy {
     async fn sync_state(&mut self) -> Result<()> {
-        todo!()
+        let counter = Counter::deploy(self.client.clone(), ())
+            .unwrap()
+            .send()
+            .await
+            .unwrap();
+        self.counter = Some(counter);
+        self.sender
+            .send(SimulationEvents::Message(
+                "Counter contract deployed".to_string(),
+            ))
+            .unwrap();
+        self.sender
+            .send(SimulationEvents::Message("Second message".to_string()))
+            .unwrap();
+        println!("contract deployed");
+        Ok(())
     }
 
-    async fn process_event(&mut self, _event: RevmResult) -> Option<()> {
-
-        let counter = Counter::deploy(self.client.clone(), ()).unwrap().send().await.unwrap();
-        println!("Deployed Counter to address: {}", counter.address());
-        Some(())
-
+    async fn process_event(&mut self, _event: SimulationEvents) -> Vec<SimulationActions> {
+        self.sender
+            .send(SimulationEvents::Message(
+                "Counter contract call".to_string(),
+            ))
+            .unwrap();
+        match _event {
+            SimulationEvents::Message(_event) => {
+                if let Some(counter) = &self.counter {
+                    let contract_call = counter.increment();
+                    println!("contract call");
+                    vec![SimulationActions::ContractSend(contract_call)]
+                } else {
+                    vec![]
+                }
+            }
+            _ => vec![],
+        }
     }
 }
