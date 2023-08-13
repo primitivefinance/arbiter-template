ce77a94d26eeaf57e7c40e0dd551fb83636fa15f
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-08-13 10:49:54 -0700

    📜✨
    simple example for using revm middleware
    - forge instead of binary
    - fix CI

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,36 @@
+use arbiter_core::manager::Manager;
 use arbiter_core::middleware::RevmMiddleware;
-use arbiter_core::{
-    manager::Manager,
-    strategies::{SimulationCollector, SimulationExecutor},
-};
-use artemis_core::engine::Engine;
-use crossbeam_channel::unbounded;
+use ethers::providers::Middleware;
 use std::{error::Error, sync::Arc};
 
 use crate::bindings::counter::Counter;
 
 mod bindings;
-mod strategies;
 
 const TEST_ENV_LABEL: &str = "test";
-const TEST_AGENT_NAME: &str = "bob";
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn Error>> {
     let mut manager = Manager::new();
-    let _ = manager.add_environment(TEST_ENV_LABEL, 1.0, 1, Engine::new());
+    let _ = manager.add_environment(TEST_ENV_LABEL, 1.0, 1);
 
-    let client = Arc::new(RevmMiddleware::new(
+    let client_with_signer = Arc::new(RevmMiddleware::new(
         manager.environments.get(TEST_ENV_LABEL).unwrap(),
     ));
+    println!("created client with address {}", client_with_signer.default_sender().unwrap());
+    manager.start_environment(TEST_ENV_LABEL)?;
 
-    let (sender, receiver) = crossbeam_channel::unbounded();
-
-    let strategy =
-        strategies::CounterStrategy::new(TEST_AGENT_NAME.to_string(), client.clone(), sender);
-    let collector = SimulationCollector::new(receiver);
-    let executor = SimulationExecutor::new(client.clone());
-
-    manager
-        .environments
-        .get_mut(TEST_ENV_LABEL)
+    let counter = Counter::deploy(client_with_signer.clone(), ())
         .unwrap()
-        .engine()
-        .add_collector(Box::new(collector));
-    manager
-        .environments
-        .get_mut(TEST_ENV_LABEL)
-        .unwrap()
-        .engine()
-        .add_strategy(Box::new(strategy));
-    manager
-        .environments
-        .get_mut(TEST_ENV_LABEL)
-        .unwrap()
-        .engine()
-        .add_executor(Box::new(executor));
+        .send()
+        .await
+        .unwrap();
+    println!("Counter contract deployed at {}", counter.address());
 
-    manager.start_environment(TEST_ENV_LABEL).await?;
+    for index in 0..10 {
+        counter.increment().call().await.unwrap();
+        println!("Counter incremented to {}", index);
+    }
 
     Ok(())
 }
